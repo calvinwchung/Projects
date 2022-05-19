beadd6bacc4b6463d1c5b9a63cf8d21f661de2be
calvin chung <calvin.w.chung@gmail.com>
2022-05-19 14:30:45 -0400

    filter functionality

---
+++ anti.js
@@ -0,0 +1,11 @@
+var theShirts = document.getElementById('shirt');
+var theHoodies = document.getElementById('hoodie');
+var theFirstRow = document.getElementsById('firstRow');
+
+function clickShirt () {
+    theHoodies.remove();
+    }
+
+// function clickShirt (theFirstRow) {
+//     theHoodies.theFirstRow.removeChild(theHoodies);
+// }
